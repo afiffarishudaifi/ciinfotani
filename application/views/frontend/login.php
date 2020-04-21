1e1a12f07a31aab5faf171999ee17d396911b66c
murpysn <nugiesatriya@gmail.com>
2020-04-21 20:34:25 +0700

    Revert "Tampilan Frontend CI"
    This reverts commit ce173ce7fa007b8726eff3456b76d328f058e140.

--- application/views/frontend/login.php
+++ application/views/frontend/login.php
@@ -1,51 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>INFOTANI - MASUK</title>
-
-    <!-- logo infotani -->
-    <link rel="icon" href="<?= base_url('assets/img/logo.png')?>">
-    <!-- Bootstrap core CSS -->
-    <link href="<?= base_url('assets/frontend/css/bootstrap.min.css')?>" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="<?= base_url('assets/frontend/css/ie10-viewport-bug-workaround.css')?>" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="<?= base_url('assets/frontend/css/signin.css')?>" rel="stylesheet">
-  </head>
-
-  <body>
-    <div class="container">
-        <form class="form-signin" id="login" name="login" action="<?php echo base_url('Login/cek_login'); ?>" method="POST">
-        <br>
-        <h2 class="form-signin-heading" align="center" style="color: #FFFFFF;">MASUK INFOTANI</h2>
-
-        <br>
-        <label for="username" class="sr-only">Nama Pengguna</label>
-        <input type="text" id="username" name="txt_user" class="form-control" placeholder="Nama Pengguna" required autofocus>
-        <br>
-        <label for="password" class="sr-only">Kata Sandi</label>
-        <input type="password" id="password" name="txt_pass" class="form-control" placeholder="Kata Sandi" required>
-        <br>
-        <input type="submit" name="btn_log" class="btn btn-primary btn-user btn-block" value="Masuk">
-        <br>
-        <a href="<?= base_url('lupa')?>" style="color:whitesmoke">Lupa Kata Sandi Anda?</a></p>
-        <center> <p>------------------- atau --------------------</p> </center>
-        <a href="<?= base_url('register')?>" class="btn btn-success btn-block">Buat Akun Baru?</a>
-        
-      	<br>
-	<a href="<?= base_url('beranda')?>" class="btn btn-warning btn-block">Kembali</a>
-      <br>
-      </form>
-</div> <!-- /container -->
-
-  </body>
-</html>
+<form class="user" action="<?php echo base_url('Login/cek_login'); ?>" method="POST">
+    <div class="form-group">
+        <input type="text" class="form-control form-control-user" id="username" aria-describedby="username" placeholder="Enter Username..." name="txt_user">
+    </div>
+    <div class="form-group">
+        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="txt_pass">
+    </div>
+    <div class="form-group">
+        <div class="custom-control custom-checkbox small">
+            <input type="checkbox" class="custom-control-input" id="customCheck">
+            <label class="custom-control-label" for="customCheck">Remember Me</label>
+        </div>
+    </div>
+    <input type="submit" name="btn_log" class="btn btn-primary btn-user btn-block" value="Login">
+    <hr>
+    <a href="index.html" class="btn btn-google btn-user btn-block">
+        <i class="fab fa-google fa-fw"></i> Login with Google
+    </a>
+    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+    </a>
+</form>
