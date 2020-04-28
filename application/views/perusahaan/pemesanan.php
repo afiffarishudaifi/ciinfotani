b445cc0a28be877915e0680d294e95cb4b680fff
afiffarishudaifi <afiffaris5@gmail.com>
2020-04-28 20:14:26 +0700

    commit ulang

--- application/views/perusahaan/pemesanan.php
+++ application/views/perusahaan/pemesanan.php
@@ -218,9 +218,6 @@ $getLogo = $this->session->userdata('session_logo_perusahaan');
 
         <!-- Control Sidebar -->
 
-        <!-- /.control-sidebar -->
-        <!-- Add the sidebar's background. This div must be placed
-       immediately after the control sidebar -->
         <div class="control-sidebar-bg"></div>
     </div>
     <!-- ./wrapper -->
