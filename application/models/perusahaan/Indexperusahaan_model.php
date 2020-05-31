45b41ec0f0ecb9c848809c374d68b870235293a9
afiffarishudaifi <afiffaris5@gmail.com>
2020-05-31 16:01:43 +0700

    perubahan tulisan

--- application/models/perusahaan/Indexperusahaan_model.php
+++ application/models/perusahaan/Indexperusahaan_model.php
@@ -21,18 +21,18 @@ class Indexperusahaan_model extends CI_Model{
         date_default_timezone_set('Asia/Jakarta');
         $tahun = date("Y");
         $data = [
-            $this->db->query("SELECT COUNT(*) as jan FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 01 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as feb FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 02 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as mar FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 03 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as apr FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 04 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as mei FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 05 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as jun FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 06 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as jul FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 07 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as ags FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 08 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as sep FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 09 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as okt FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 10 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as nov FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 11 and year(TANGGAL) = $tahun")->result_array(),
-            $this->db->query("SELECT COUNT(*) as dess FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 12 and year(TANGGAL) = $tahun")->result_array()
+            $this->db->query("SELECT COUNT(*) as jan FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 01 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as feb FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 02 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as mar FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 03 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as apr FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 04 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as mei FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 05 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as jun FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 06 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as jul FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 07 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as ags FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 08 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as sep FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 09 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as okt FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 10 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as nov FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 11 and year(TANGGAL) = $tahun")->result_array(),
+            $this->db->query("SELECT COUNT(*) as dess FROM pemesanan WHERE ID_PERUSAHAAN= $id_pengguna AND month(TANGGAL) = 12 and year(TANGGAL) = $tahun")->result_array()
         ];
         return $data;
     }
